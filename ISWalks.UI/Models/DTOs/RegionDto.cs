d26d3902f92fca1eb09cc4b940472350f19b2aae
faranak <sfaranak52@gmail.com>
2024-07-06 20:30:25 +0330

    consume APIs

---
+++ ISWalks.UI/Models/DTOs/RegionDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ISWalks.UI.Models.DTOs
+{
+    public class RegionDto
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string? RegionImgUrl { get; set; }
+    }
+}
